d8190a3b302d364543e4323947cb35c4e5e45a08
Henrytos <henry.arcaya@sptech.school>
2026-04-22 17:32:35 -0300

    feat: estrutura inicial

---
+++ src/components/Aside.jsx
@@ -0,0 +1,86 @@
+import { ArrowLeftRight, Calendar, Home } from "lucide-react";
+
+export default function AsideBar() {
+  const links = [
+    {
+      path: "/dashboard",
+      name: "Paginal Inicial",
+      target: true,
+      icon: <Home />,
+    },
+    {
+      path: "/AgendamentosPage",
+      name: "Agendamento",
+      target: false,
+      icon: <Calendar />,
+    },
+    {
+      path: "/dashboard-financeiro",
+      name: "Financeiro",
+      target: false,
+      icon: <ArrowLeftRight />,
+    },
+  ];
+
+  return (
+    <aside className="w-1/7 min-w-[256px] bg-[#0A1A3D] py-6 flex flex-col">
+      <div className="px-6 mb-10">
+        <h2 className="font-bold text-xl ">KORE</h2>
+
+        <p className="opacity-60 text-xs font-normal">
+          Gerenciamento de estúdio
+        </p>
+      </div>
+
+      <div className="flex flex-col font-medium flex-grow">
+        {links.map((link, index) => (
+          <AsideLink
+            key={link.path}
+            href={link.path}
+            isTarget={link.target}
+            icon={link.icon}
+          >
+            {link.name}
+          </AsideLink>
+        ))}
+      </div>
+
+      <AsideFooter />
+    </aside>
+  );
+}
+
+function AsideLink({ href, isTarget, icon, children }) {
+  return (
+    <a
+      href={href}
+      className={`flex gap-3 items-center justify-start py-3 px-4 ${isTarget ? "opacity-100 text-[#0CC0DF] border-l-4 border-[#0CC0DF] bg-[#263457]/30" : "opacity-60"} hover:opacity-100  hover:text-[#0CC0DF]  hover:border-l-4  hover:border-[#0CC0DF]  hover:bg-[#263457]/30 transition-all`}
+    >
+      <span>{icon}</span>
+      <span>{children}</span>
+    </a>
+  );
+}
+
+function AsideFooter() {
+  return (
+    <footer className="flex items-center gap-2 text-xs opacity-40 justify-center">
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 7H6C6.14167 7 6.26042 6.95208 6.35625 6.85625C6.45208 6.76042 6.5 6.64167 6.5 6.5V5.5H5.5V6H4.5V4H5.5V4.5H6.5V3.5C6.5 3.35833 6.45208 3.23958 6.35625 3.14375C6.26042 3.04792 6.14167 3 6 3H4C3.85833 3 3.73958 3.04792 3.64375 3.14375C3.54792 3.23958 3.5 3.35833 3.5 3.5V6.5C3.5 6.64167 3.54792 6.76042 3.64375 6.85625C3.73958 6.95208 3.85833 7 4 7ZM5 10C4.30833 10 3.65833 9.86875 3.05 9.60625C2.44167 9.34375 1.9125 8.9875 1.4625 8.5375C1.0125 8.0875 0.65625 7.55833 0.39375 6.95C0.13125 6.34167 0 5.69167 0 5C0 4.30833 0.13125 3.65833 0.39375 3.05C0.65625 2.44167 1.0125 1.9125 1.4625 1.4625C1.9125 1.0125 2.44167 0.65625 3.05 0.39375C3.65833 0.13125 4.30833 0 5 0C5.69167 0 6.34167 0.13125 6.95 0.39375C7.55833 0.65625 8.0875 1.0125 8.5375 1.4625C8.9875 1.9125 9.34375 2.44167 9.60625 3.05C9.86875 3.65833 10 4.30833 10 5C10 5.69167 9.86875 6.34167 9.60625 6.95C9.34375 7.55833 8.9875 8.0875 8.5375 8.5375C8.0875 8.9875 7.55833 9.34375 6.95 9.60625C6.34167 9.86875 5.69167 10 5 10ZM5 9C6.11667 9 7.0625 8.6125 7.8375 7.8375C8.6125 7.0625 9 6.11667 9 5C9 3.88333 8.6125 2.9375 7.8375 2.1625C7.0625 1.3875 6.11667 1 5 1C3.88333 1 2.9375 1.3875 2.1625 2.1625C1.3875 2.9375 1 3.88333 1 5C1 6.11667 1.3875 7.0625 2.1625 7.8375C2.9375 8.6125 3.88333 9 5 9Z"
+          fill="#DAE2FF"
+          fill-opacity="0.4"
+        />
+      </svg>
+      <p>
+        Desenvolvido por: KORE © <br /> {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
--- src/pages/AgendamentosPage.jsx
+++ src/pages/AgendamentosPage.jsx
@@ -1,7 +1,194 @@
-export default function AgendamentosPage() {
+import { differenceInHours } from "date-fns";
+import {
+  ArrowLeftRight,
+  Calendar,
+  CalendarPlus,
+  CirclePlus,
+  Clock,
+  Home,
+} from "lucide-react";
+import { useState } from "react";
+
+import { DayPicker } from "react-day-picker";
+
+import { ptBR } from "react-day-picker/locale";
+
+import "react-day-picker/style.css";
+import AsideBar from "../components/Aside";
+
+export default function AgendamentoPage() {
+  const [selected, setSelected] = useState(new Date());
+
+  console.log(selected);
+
+  // struct
+  // id;
+  // preco;
+  // cliente;
+  // telefone;
+  // formaPagamento;
+  // inicio;
+  // fim;
+
+  const currentDate = new Date();
+
+  const agendamentos = [
+    {
+      id: 1,
+      preco: 20.0,
+      cliente: "henry",
+      telefone: "123456789",
+      formaPagamento: "PIX",
+      inicio: currentDate.setHours(currentDate.getHours() + 2),
+      fim: currentDate.setHours(currentDate.getHours() + 4),
+    },
+    {
+      id: 2,
+      preco: 30.0,
+      cliente: "vitor",
+      telefone: "123456789",
+      formaPagamento: "PIX",
+      inicio: currentDate.setHours(currentDate.getHours() + 6),
+      fim: currentDate.setHours(currentDate.getHours() + 8),
+    },
+
+    {
+      id: 3,
+      preco: 40.0,
+      cliente: "diogo",
+      telefone: "123456789",
+      formaPagamento: "PIX",
+      inicio: currentDate.setHours(currentDate.getHours() + 10),
+      fim: currentDate.setHours(currentDate.getHours() + 12),
+    },
+    {
+      id: 4,
+      preco: 40.0,
+      cliente: "diogo",
+      telefone: "123456789",
+      formaPagamento: "PIX",
+      inicio: currentDate.setHours(currentDate.getHours() + 10),
+      fim: currentDate.setHours(currentDate.getHours() + 12),
+    },
+  ];
+
+  const formatCurrecy = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
   return (
-    <main>
-      <h1>Agendamentos</h1>
+    <main className="h-screen w-full flex bg-[#000C24] text-[#DAE2FF]">
+      <AsideBar />
+
+      <section className="flex-grow h-full ">
+        <div className="p-6 flex w-full justify-between">
+          <h1 className="text-4xl font-bold">AGENDAMENTO</h1>
+
+          <button className="flex gap-2 px-6 py-2.5 bg-linear-to-r from-[#48DCFC] to-[#0CC0DF] text-[#003640] rounded-xl shadow-xl shadow-cyan-500/20 cursor-pointer">
+            <CalendarPlus />
+            Agendar
+          </button>
+        </div>
+
+        <div className="flex px-6 gap-6 flex-grow  h-[85%]">
+          {/* AGENDA */}
+          <div className="flex-grow h-full">
+            <DayPicker
+              className="mb-6"
+              animate
+              mode="single"
+              selected={selected}
+              onSelect={setSelected}
+              locale={ptBR}
+              classNames={{
+                selected: `text-[#0CC0DF]`, // Highlight the selected day
+                chevron: `fill-[#0CC0DF]`, // Change the color of the chevron
+              }}
+            />
+
+            <h2 className="font-semibold text-lg mb-3">Proximas Sessões</h2>
+
+            <div className=" flex flex-col gap-3">
+              {agendamentos.map((agendamento) => (
+                <div
+                  key={agendamento.id}
+                  className="flex bg-[#0A1A3D] border border-[#0CC0DF]/10 w-full p-3 justify-between rounded-xl"
+                >
+                  <div className="flex gap-4 items-center">
+                    <span className="w-10 h-10 rounded-full bg-[#48DCFC]/10 flex items-center justify-center">
+                      <CirclePlus
+                        className="text-[#48DCFC]"
+                        width={20}
+                        height={20}
+                      />
+                    </span>
+                    <div>
+                      <h4 className=" font-semibold">{agendamento.cliente}</h4>
+                      <p className="text-xs text-[#BBC9CD] font-normal">
+                        Em {differenceInHours(agendamento.inicio, new Date())}{" "}
+                        horas
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col items-end gap-1">
+                    <p className="text-[#48DCFC] font-bold">
+                      {formatCurrecy.format(agendamento.preco)}
+                    </p>
+                    <p className="text-[#48DCFC] font-bold text-[10px] bg-[#48DCFC]/10 px-2 py-1 rounded-full w-fit">
+                      {agendamento.formaPagamento}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* CALENDARIO */}
+
+          <section className="flex-grow h-full w-full  grid grid-cols-12 border grid-rows-10 border-[#3C494D]/10 rounded-3xl ">
+            <div className="col-start-1 col-end-2 row-span-10 grid grid-rows-10">
+              <div className=" flex justify-center items-center border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50 rounded-tl-3xl ">
+                <Clock width={15} height={15} />
+              </div>
+            </div>
+
+            <div className="w-full   rounded-3xl grid grid-cols-7 grid-rows-10 h-full col-start-2 col-end-13 row-start-1 row-end-11">
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#48DCFC] font-bold text-xs">SEG</p>
+                <p className="font-bold text-lg">05</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#BBC9CD] font-bold text-xs">TER</p>
+                <p className="font-bold text-lg">06</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#BBC9CD] font-bold text-xs">QUA</p>
+                <p className="font-bold text-lg">07</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#BBC9CD] font-bold text-xs">QUI</p>
+                <p className="font-bold text-lg">08</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#BBC9CD] font-bold text-xs">SEX</p>
+                <p className="font-bold text-lg">09</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50">
+                <p className="text-[#BBC9CD] font-bold text-xs">SAB</p>
+                <p className="font-bold text-lg">10</p>
+              </div>
+              <div className=" flex justify-center items-center flex-col gap-0 border-b border-r border-[#3C494D]/10 bg-[#1A294C]/50 rounded-tr-3xl">
+                <p className="text-[#BBC9CD] font-bold text-xs">DOM</p>
+                <p className="font-bold text-lg">11</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
     </main>
   );
 }
