0f340e3149714133f508af92ff0057896b123533
Henrytos <henry.arcaya@sptech.school>
2026-05-31 07:53:40 -0300

    resolvendo bug de horario

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ModalAgendamentoContextProvider>
       <RouterProvider router={router} />
-      <Toaster />
+      <Toaster />prova
     </ModalAgendamentoContextProvider>
   </StrictMode>,
 );
