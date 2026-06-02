b6ff49635a78db587e84778b138d3929b02efc83
vitor <vitor.restini@sptech.school>
2026-06-01 21:39:19 -0300

    fix: token sendo passado.

--- src/pages/Estoque/EstoquePage.jsx
+++ src/pages/Estoque/EstoquePage.jsx
@@ -15,6 +15,7 @@ export default function EstoquePage() {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
             },
         })
             .then((response) => response.json())
