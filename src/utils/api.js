2e51ace1e73d2ceb5a3381a43aad74674999adc0
AnaMioki <ana.mioki@sptech.school>
2026-06-05 21:54:31 -0300

    feat: adiciona interceptor para redirecionar ao login quando token expirar

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,14 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:8080",
 });
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401 || error.response?.status === 403) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
