b99c3def4f2a3fbbe1d869670f6edae5c4b89595
GMiraaa <guilherme.mira@sptech.school>
2026-06-06 22:30:40 -0300

    OK: Add: Adicionada a função de retração da side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   ArrowLeftRight,
   Bell,
@@ -5,13 +6,18 @@ import {
   Home,
   Package,
   PiggyBank,
+  PanelLeftClose,
+  PanelLeftOpen,
 } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
+import { useSidebar } from "../context/SidebarContext";
 
 import { Logo } from "./Logo";
 
 export default function Sidebar() {
   const location = useLocation();
+
+  const { collapsed, setCollapsed } = useSidebar();
 
   const links = [
     { path: "/dashboard", name: "Página Inicial", icon: <Home /> },
@@ -24,11 +30,27 @@ export default function Sidebar() {
 
   return (
     <>
-      <div className="flex h-screen w-64 shrink-0"></div>
+      <div className={`flex h-screen shrink-0 transition-all duration-300 ${
+          collapsed ? "w-20" : "w-64"
+        }`}
+      />
 
-      <aside className="fixed top-0 flex h-screen w-64 flex-col border-r border-gray-800 bg-[#061639] px-5 py-10 text-white">
-        <h1 className="mb-10 flex flex-col justify-center text-center text-4xl font-normal">
-          <Logo className={"h-25 w-auto object-contain"} />
+      <aside className={`fixed top-0 flex h-screen flex-col border-r border-gray-800 bg-[#061639] px-3 py-10 text-white transition-all duration-300 ${
+          collapsed ? "w-20" : "w-64"
+        }`}
+      >
+
+      <button onClick={() => setCollapsed(!collapsed)}
+        className="absolute top-4 right-4 rounded-lg p-2 hover:bg-white/10"
+      >
+        {collapsed ? <PanelLeftOpen size={20} /> : <PanelLeftClose size={20} />}
+      </button>
+
+        <h1 className={`mb-10 flex justify-center transition-all ${
+            collapsed ? "opacity-0 h-0 overflow-hidden" : ""
+          }`}
+        >
+          <Logo className="h-25 w-auto object-contain" />
         </h1>
 
         <div className="flex flex-col gap-4">
@@ -36,20 +58,31 @@ export default function Sidebar() {
             <Link
               key={index}
               to={link.path}
-              className={`flex items-center gap-3 rounded-lg px-4 py-3 transition-all ${
+              className={`flex items-center rounded-lg px-4 py-3 transition-all ${
+                collapsed ? "justify-center" : "gap-3"
+              } ${
                 location.pathname === link.path
                   ? "border-l-4 border-cyan-400 bg-cyan-500/20 text-cyan-400"
                   : "hover:bg-white/5"
               }`}
             >
               {link.icon}
-              {link.name}
+
+              <span
+                className={`overflow-hidden whitespace-nowrap transition-all duration-300 ${
+                  collapsed
+                    ? "max-w-0 opacity-0"
+                    : "max-w-[200px] opacity-100"
+                }`}
+              >
+                {link.name}
+              </span>
             </Link>
           ))}
         </div>
 
         <footer className="mt-auto text-center text-xs opacity-30">
-          DESENVOLVIDO POR: KORE © 2026
+          {!collapsed && (<p >DESENVOLVIDO POR: KORE © 2026</p>)}
         </footer>
       </aside>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import "./index.css";
 
 import { ModalAgendamentoContextProvider } from "./context/ModalAgendamentoContext";
+import { SidebarProvider } from "./context/SidebarContext";
 import { RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
 import { Toaster } from "sonner";
 import { createRoot } from "react-dom/client";
 import { router } from "./router";
 
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ModalAgendamentoContextProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ModalAgendamentoContextProvider>
-  </StrictMode>,
+    <SidebarProvider>
+      <ModalAgendamentoContextProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </ModalAgendamentoContextProvider>
+    </SidebarProvider>
+  </StrictMode>
 );
